b9161079eee55af8185b2bc5ee581efa237d6fe4
lucasbarross <lbam@cin.ufpe.br>
2019-06-16 21:43:50 -0300

    Add playIO call

--- src/Entities/Draw.hs
+++ src/Entities/Draw.hs
@@ -14,8 +14,8 @@ module Entities.Draw (render, background, window) where
     background :: Color
     background = black
 
-    render :: Game -> Picture
-    render g = pictures ([(color green $ (Line [(-500, 0), (500, 0)])), renderPlayer (player g)] ++  map (renderObstacle) (obstacles g))
+    render :: Game -> IO(Picture)
+    render g = return (pictures ([(color green $ (Line [(-500, 0), (500, 0)])), renderPlayer (player g)] ++  map (renderObstacle) (obstacles g)))
 
     renderPlayer :: (Float, Float) -> Picture
     renderPlayer (x, y) = translate (x) (y) $ color red $ rectangleSolid 20 40
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,28 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Game
+
 import Entities.Game
 import Entities.Draw
 
+handleInput :: Event -> Game -> IO (Game)
+handleInput (EventKey (Char 'w') (Down) _ _) game = return Game { player = (0, 100), obstacles = (obstacles game) }
+handleInput _ g = return g
+
+stepGame :: Float -> Game -> IO (Game)
+stepGame _ game = return Game { player = defaultPlayerPos, obstacles = (obstacles game)}
+
 main :: IO ()
-main = display window background (render restartGame) 
+main = do
+  playIO
+    (InWindow "J-Rex" (500, 500) (500, 500))
+    white
+    10
+    restartGame
+    render
+    handleInput
+    stepGame
 
 
