32a632905ba1214a1f2ec0955e22e68ab36d70fb
gabrielpessoa1 <gap@cin.ufpe.br>
2019-06-27 00:08:53 -0300

    refacto collision to avoid sqrt

--- src/Entities/Game.hs
+++ src/Entities/Game.hs
@@ -13,6 +13,12 @@ module Entities.Game where
     obstacleHeight :: Float
     obstacleHeight = 30
     
+    xCollisionDistance :: Float
+    xCollisionDistance = playerRadius + (obstacleWidth / 2)
+
+    yCollisionDistance :: Float
+    yCollisionDistance = playerRadius + (obstacleHeight / 2)
+
     defaultPlayerPos :: (Float, Float)
     defaultPlayerPos = (0, 0)
 
@@ -33,14 +39,14 @@ module Entities.Game where
 
         return ()
 
-    geometricDistance :: (Float, Float) -> (Float, Float) -> Float
-    geometricDistance (x1, y1) (x2, y2) = sqrt ((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    geometricDistanceSquared :: (Float, Float) -> (Float, Float) -> Float
+    geometricDistanceSquared (x1, y1) (x2, y2) = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 
     testCollision :: (Float, Float) -> (Float, Float) -> Bool
     testCollision a b = xCollison && yCollision
-        where xCollison = distance < playerRadius + (obstacleWidth / 2)
-              yCollision = distance < playerRadius + (obstacleHeight / 2)
-              distance = (geometricDistance a b)
+        where xCollison = distanceSquared < xCollisionDistance * xCollisionDistance
+              yCollision = distanceSquared < yCollisionDistance * yCollisionDistance
+              distanceSquared = (geometricDistanceSquared a b)
             
     hasCollision :: (Float, Float) -> [(Float, Float)] -> Bool
     hasCollision playerPos obstacles = (any (testCollision (playerPos)) obstacles)
